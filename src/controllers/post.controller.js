948ab377309f45c754b0d7b195c6879ae1b69f5e
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-14 18:19:32 +0530

    ai generated caption feature added

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,8 +1,15 @@
 const postModel = require('../models/post.model')
-
+const generateCaption = require('../service/ai.service')
 async function createPostController(req,res){
     const file = req.file;
+    console.log('file recieved', file)
+
+    const base64Image = new Buffer.from(file.buffer).toString('base64')
+    const caption = await generateCaption(base64Image) 
     
+    res.json({
+        caption
+    })
 }
 
-module.exports = {createPostController}
+module.exports = {createPostController} 
--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -3,12 +3,35 @@ const { GoogleGenAI } = require("@google/genai");
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({});
 
-async function main() {
+async  function generateCaption(base64ImageFile){
+
+  
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: base64ImageFile,
+      },
+    },
+    { text: "Caption this image." },
+  ];
+  
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: contents,
+    config:{
+      systemInstruction:`
+      You are an expert in generating captions for images.
+      You generate single caption for the image. 
+      Your caption should be short and concise.
+      You use hashtags and emojis in the caption. 
+      Generate caption using tapori wayh.
+      The caption should be in dark humour.
+      `
+    }
   });
-  console.log(response.text);
+  return response.text
 }
 
-main();
+
+module.exports = generateCaption;
